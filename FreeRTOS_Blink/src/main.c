4bc6c2b55ab8b514e2690d8e7a6d2a98cb0f80f9
viduraembedded <viduara.embedded@gmail.com>
2021-06-12 12:18:37 +0530

    Modified the FreeRTOS project with 2 Tasks
    Signed-off-by: viduraembedded <viduara.embedded@gmail.com>

--- FreeRTOS_Blink/src/main.c
+++ FreeRTOS_Blink/src/main.c
@@ -7,39 +7,48 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-void vGreenLEDTask( void * pvParameters )
-{
-    /* The parameter value is expected to be 1 as 1 is passed in the
-    pvParameters value in the call to xTaskCreate() below. 
-    configASSERT( ( ( uint32_t ) pvParameters ) == 1 );
-    */
-    for( ;; )
-    {
-        gpio_put(25, 1);
-        vTaskDelay(1000);
-        gpio_put(25, 0);
-        vTaskDelay(1000);
-    }
-}
+//Define Macros here
+#define RED_LED_PIN 16
+#define YELLOW_LED_PIN 17
 
+//Define function prototyoes here
+void vRedLEDTask( void * pvParameters );
+void vYellowLEDTask( void * pvParameters );
+
+// Main function implementation
 int main() {
 
 stdio_init_all();
 
-gpio_init(25);
-gpio_set_dir(25, GPIO_OUT);
+gpio_init(RED_LED_PIN);
+gpio_init(YELLOW_LED_PIN);
+gpio_set_dir(RED_LED_PIN, GPIO_OUT);
+gpio_set_dir(YELLOW_LED_PIN, GPIO_OUT);
+
 
 BaseType_t xReturned;
-TaskHandle_t xHandle = NULL;
+
+TaskHandle_t xRedLEDHandle = NULL;
+TaskHandle_t xYellowLEDHandle = NULL;
+
 /* Create the task, storing the handle. */
     xReturned = xTaskCreate(
-                    vGreenLEDTask,       /* Function that implements the task. */
-                    "Blinky Task",   /* Text name for the task. */
+                    vRedLEDTask,       /* Function that implements the task. */
+                    "RED LED Task",   /* Text name for the task. */
                     512,             /* Stack size in words, not bytes. */
                     NULL,            /* Parameter passed into the task. */
-                    tskIDLE_PRIORITY,/* Priority at which the task is created. */
-                    &xHandle );   
+                    tskIDLE_PRIORITY + 1,/* Priority at which the task is created. */
+                    &xRedLEDHandle );   
 
+    xReturned = xTaskCreate(
+                    vYellowLEDTask,       /* Function that implements the task. */
+                    "Yellow LED Task",   /* Text name for the task. */
+                    512,             /* Stack size in words, not bytes. */
+                    NULL,            /* Parameter passed into the task. */
+                    tskIDLE_PRIORITY + 1,/* Priority at which the task is created. */
+                    &xYellowLEDHandle ); 
+
+                    
     vTaskStartScheduler();
     
     while(1)
@@ -47,4 +56,37 @@ TaskHandle_t xHandle = NULL;
         //configASSERT(0);    /* We should never get here */
     }
 
+}
+/*********************************************************
+*Task Implementation 
+**********************************************************
+*/
+
+// Red LED task implementation /functionality 
+void vRedLEDTask( void * pvParameters )
+{
+    (void) pvParameters;
+
+    for( ;; )
+    {
+        gpio_put(RED_LED_PIN, 1);
+        vTaskDelay(500);
+        gpio_put(RED_LED_PIN, 0);
+        vTaskDelay(500);
+    }
+}
+
+// Yellow LED task implementation/functionality 
+void vYellowLEDTask( void * pvParameters )
+{
+
+    (void) pvParameters;
+
+    for( ;; )
+    {
+        gpio_put(YELLOW_LED_PIN, 1);
+        vTaskDelay(1000);
+        gpio_put(YELLOW_LED_PIN, 0);
+        vTaskDelay(1000);
+    }
 }
